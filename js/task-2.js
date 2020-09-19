b657590e9a8ab8143ff460a83cf5ee5241efd640
alisova-projects <63472862+alisova-projects@users.noreply.github.com>
2020-09-19 10:05:09 +0300

    update

---
+++ js/task-2.js
@@ -0,0 +1,43 @@
+/*
+В HTML есть пустой список ul#ingredients:
+<ul id="ingredients"></ul>
+
+В JS есть массив строк:
+
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+Напиши скрипт, который для каждого элемента массива ingredients создаст 
+отдельный li,
+после чего вставит все li за одну операцию в список ul.ingredients.
+Для создания DOM - узлов используй document.createElement().
+*/
+
+const ingredients = [
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
+];
+
+const ingredientsContainerEl = document.querySelector("#ingredients");
+// console.log(ingredientsContainerEl);
+
+const dish = ingredients.map((ingredient) => {
+  const liEl = document.createElement("li");
+  liEl.textContent = ingredient;
+
+  return liEl;
+});
+
+console.log(dish);
+
+ingredientsContainerEl.append(...dish);
---
+++ js/task-3.js
@@ -0,0 +1,88 @@
+/*
+Напиши скрипт для создания галлереи изображений по массиву данных.
+
+В HTML есть список ul#gallery:
+<ul id="gallery"></ul>
+
+Используй массив объектов images для создания тегов img вложенных в li.
+Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+
+Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+Добавь минимальное оформление галереи флексбоксами или гридами через 
+css-классы.
+
+const images = [
+  {
+    url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+*/
+const galleryContainerEl = document.querySelector("ul#gallery");
+console.log(galleryContainerEl);
+
+const images = [
+  {
+    url:
+      "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
+
+// const makeGalleryItem = ({ url, alt }) => {
+//   const galleryItemEl = document.createElement("li");
+//   const galleryImgEl = document.createElement("img");
+//   galleryImgEl.alt = alt;
+//   galleryImgEl.src = url;
+//   galleryImgEl.width = 320;
+
+//   galleryItemEl.append(galleryImgEl);
+
+//   return galleryItemEl;
+// };
+
+// const elements = images.map(makeGalleryItem);
+
+// galleryContainerEl.append(...elements);
+
+const GALLERY_CLASS = "image-gallery";
+const GALLERY_ITEM_CLASS = "image-gallery__item";
+const GALLERY_IMAGE_CLASS = "image-gallery__image";
+
+const createGalleryMarkup = (img) => {
+  return img.reduce(
+    (acc, { url, alt }) =>
+      acc +
+      `<li class='${GALLERY_ITEM_CLASS}'><img class='${GALLERY_IMAGE_CLASS}' src='${url}' alt='${alt}'></li>`,
+    ""
+  );
+};
+
+const galleryMarkup = createGalleryMarkup(images);
+
+const galleryEl = document.querySelector("#gallery");
+galleryContainerEl.classList.add(GALLERY_CLASS);
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+// console.log(galleryContainerEl.innerHTML);
